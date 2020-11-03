d7723bbef36f8266fd5994cafc9b7b684155394a
Cancio <filipe.cancio@outlook.com>
2020-11-02 21:49:34 -0300

    Criando a primeira versao da api #1

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const example = require('./data/example.json');
 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(request,response) => {
+    return response.json(example);
+});
+
+app.listen(8080, () => {
+  console.log(`App listening on port http://localhost:8080/`);
+  console.log('Press Ctrl+C to quit.');
+});
